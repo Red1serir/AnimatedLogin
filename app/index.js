59114453cb266a0756ca7b377c47174c09d088f2
Red1 <meritatan964@gmail.com>
2021-03-31 21:36:08 +0100

    adding animation

--- app/index.js
+++ app/index.js
@@ -1,8 +1,91 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 
+import Animated, { Easing } from 'react-native-reanimated';
+import { TapGestureHandler, State } from 'react-native-gesture-handler';
 const { width, height } = Dimensions.get('window');
+
+const {
+  Value,
+  event,
+  block,
+  cond,
+  eq,
+  set,
+  Clock,
+  startClock,
+  stopClock,
+  debug,
+  timing,
+  clockRunning,
+  interpolate,
+  Extrapolate
+} = Animated;
+
+function runTiming(clock, value, dest) {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0)
+  };
+
+  const config = {
+    duration: 1000,
+    toValue: new Value(0),
+    easing: Easing.inOut(Easing.ease)
+  };
+
+  return block([
+    cond(clockRunning(clock), 0, [
+      set(state.finished, 0),
+      set(state.time, 0),
+      set(state.position, value),
+      set(state.frameTime, 0),
+      set(config.toValue, dest),
+      startClock(clock)
+    ]),
+    timing(clock, state, config),
+    cond(state.finished, debug('stop clock', stopClock(clock))),
+    state.position
+  ]);
+}
 class TESTAPP extends Component {
+  constructor() {
+    super();
+
+    this.buttonOpacity = new Value(1);
+
+    this.onStateChange = event([
+      {
+        nativeEvent: ({ state }) =>
+          block([
+            cond(
+              eq(state, State.END),
+              set(this.buttonOpacity, runTiming(new Clock(), 1, 0))
+            )
+          ])
+      }
+    ]);
+
+    this.buttonY = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [100, 0],
+      extrapolate: Extrapolate.CLAMP
+    });
+
+    this.bgY = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [-height / 3, 0],
+      extrapolate: Extrapolate.CLAMP
+    });
+    
+    this.raduis = interpolate(this.buttonOpacity, {
+        inputRange: [0, 1],
+        outputRange: [50, 0],
+        extrapolate: Extrapolate.CLAMP
+      });
+  }
   render() {
     return (
       <View
@@ -12,21 +95,42 @@ class TESTAPP extends Component {
           justifyContent: 'flex-end'
         }}
       >
-        <View style={{ ...StyleSheet.absoluteFill }}>
-          <Image
+        <Animated.View
+          style={{
+            ...StyleSheet.absoluteFill,
+            transform: [{ translateY: this.bgY }]
+           
+          }}
+        >
+          <Animated.Image
             source={require('../assets/bg.jpg')}
-            style={{ flex: 1, height: null, width: null }}
+            style={{ flex: 1, height: null, width: null,borderBottomLeftRadius:this.raduis,borderBottomRightRadius:this.raduis }}
           />
-        </View>
+        </Animated.View>
         <View style={{ height: height / 3, justifyContent: 'center' }}>
-          <View style={styles.button}>
-            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>SIGN IN</Text>
-          </View>
-          <View style={{ ...styles.button, backgroundColor: '#2E71DC' }}>
+          <TapGestureHandler onHandlerStateChange={this.onStateChange}>
+            <Animated.View
+              style={{
+                ...styles.button,
+                opacity: this.buttonOpacity,
+                transform: [{ translateY: this.buttonY }]
+              }}
+            >
+              <Text style={{ fontSize: 20, fontWeight: 'bold' }}>SIGN IN</Text>
+            </Animated.View>
+          </TapGestureHandler>
+          <Animated.View
+            style={{
+              ...styles.button,
+              backgroundColor: '#2E71DC',
+              opacity: this.buttonOpacity,
+              transform: [{ translateY: this.buttonY }]
+            }}
+          >
             <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white' }}>
               SIGN IN WITH FACEBOOK
             </Text>
-          </View>
+          </Animated.View>
         </View>
       </View>
     );
